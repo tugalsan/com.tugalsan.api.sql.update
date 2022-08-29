b8546c902d62557487a9e6ccb9e18f602bc8a02a
tugalsan karabacak <tugalsan@gmail.com>
2022-08-29 12:08:55 +0300

    api.log dep code update

--- src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdateStmtUtils.java
+++ src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdateStmtUtils.java
@@ -9,7 +9,7 @@ import com.tugalsan.api.sql.conn.server.*;
 
 public class TS_SQLUpdateStmtUtils {
 
-    final private static TS_Log d = TS_Log.of(TS_SQLUpdateStmtUtils.class.getSimpleName());
+    final private static TS_Log d = TS_Log.of(TS_SQLUpdateStmtUtils.class);
 
     public static int update(TS_SQLConnAnchor anchor, CharSequence sqlStmt, List<String> colNames, List params) {
         TGS_Pack1<Integer> pack = new TGS_Pack1(0);
