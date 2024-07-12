d8599d2d7fd3e5a9f925647028c2383e7a6268d5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-12 08:45:36 +0300

    Update TS_SQLUpdateExecutor.java

--- src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdateExecutor.java
+++ src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdateExecutor.java
@@ -4,12 +4,15 @@ import java.sql.*;
 import java.util.*;
 import java.util.stream.*;
 import com.tugalsan.api.list.client.*;
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.tuple.client.*;
 import com.tugalsan.api.sql.conn.server.*;
 import com.tugalsan.api.sql.sanitize.server.*;
 import com.tugalsan.api.sql.where.server.*;
 
 public class TS_SQLUpdateExecutor {
+
+    public static TS_Log d = TS_Log.of(TS_SQLUpdateExecutor.class);
 
     public TS_SQLUpdateExecutor(TS_SQLConnAnchor anchor, CharSequence tableName) {
         this.anchor = anchor;
@@ -36,7 +39,9 @@ public class TS_SQLUpdateExecutor {
         if (where != null) {
             sb.append(" ").append(where);
         }
-        return sb.toString();
+        var str = sb.toString();
+        d.ci(tableName, str);
+        return str;
     }
 
     private int set_fill(PreparedStatement fillStmt, int offset) {
