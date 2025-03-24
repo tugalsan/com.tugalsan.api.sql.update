e452c5e05e2a5bdec34c931498d763d62cc70191
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-24 09:12:23 +0300

    .

--- src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdate.java
+++ src/main/java/com/tugalsan/api/sql/update/server/TS_SQLUpdate.java
@@ -1,6 +1,6 @@
 package com.tugalsan.api.sql.update.server;
 
-import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_In1;
+import com.tugalsan.api.function.client.maythrowexceptions.unchecked.TGS_FuncMTU_In1;
 import java.util.*;
 
 import com.tugalsan.api.tuple.client.*;
@@ -13,7 +13,7 @@ public class TS_SQLUpdate {
     }
     private final TS_SQLUpdateExecutor executor;
 
-    public TS_SQLUpdateSet set(TGS_FuncMTUCE_In1<List<TGS_Tuple2<String, Object>>> set) {
+    public TS_SQLUpdateSet set(TGS_FuncMTU_In1<List<TGS_Tuple2<String, Object>>> set) {
         set.run(executor.set);
         return new TS_SQLUpdateSet(executor);
     }
